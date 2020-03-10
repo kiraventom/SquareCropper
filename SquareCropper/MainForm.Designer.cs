a5704d7b2764b8dc986d09b811b52a55e9abf051
Kira Ventom <kiraventom@gmail.com>
2020-03-10 22:18:13 +0300

    Add project files.

---
+++ SquareCropper/MainForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace SquareCropper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainPB = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainPB
+            // 
+            this.MainPB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPB.Location = new System.Drawing.Point(0, 0);
+            this.MainPB.Name = "MainPB";
+            this.MainPB.Size = new System.Drawing.Size(682, 657);
+            this.MainPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.MainPB.TabIndex = 0;
+            this.MainPB.TabStop = false;
+            this.MainPB.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainPB_DragDrop);
+            this.MainPB.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainPB_DragEnter);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 657);
+            this.Controls.Add(this.MainPB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SquareCropper";
+            ((System.ComponentModel.ISupportInitialize)(this.MainPB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MainPB;
+    }
+}
+
---
+++ SquareCropper/MainForm.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SquareCropper
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            MainPB.AllowDrop = true;
+            PathToImage = null;
+        }
+
+        private string _pathToImage;
+        private string PathToImage 
+        {
+            get
+            {
+                return _pathToImage;
+            }
+            set
+            {
+                _pathToImage = value;
+                if (_pathToImage != null)
+                {
+                    Bitmap orig = new Bitmap(_pathToImage);
+                    Size newSize = AdjustImageSizeToSquareControl(orig.Size, MainPB.Size);
+                    Bitmap thumb = new Bitmap(orig, newSize);
+                    orig.Dispose();
+                    MainPB.Image = thumb;
+                }
+            }
+        }
+
+        private void MainPB_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void MainPB_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetData(DataFormats.FileDrop) is string[] pathsToImages)
+            {
+                string pathToImage = pathsToImages[0];
+                if (File.Exists(pathToImage))
+                {
+                    Bitmap image = new Bitmap(1, 1);
+                    try
+                    {
+                        image = new Bitmap(pathToImage);
+                    }
+                    catch (ArgumentException)
+                    {
+                        MessageBox.Show("Некорректный тип файла!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    finally
+                    {
+                        image.Dispose();
+                    }
+
+                    PathToImage = pathToImage;
+                }
+            }
+        }
+
+        public static Size AdjustImageSizeToSquareControl(Size imageSize, Size controlSize)
+        {
+            double ratio = imageSize.Width / (double)imageSize.Height;
+
+            //if pic is horizontal
+            if (ratio > 1)
+            {
+                int newWidth = controlSize.Width;
+                int newHeight = (int)(imageSize.Height * (newWidth / (double)imageSize.Width));
+                return new Size(newWidth, newHeight);
+            }
+            else
+            //if pic is vertical
+            if (ratio < 1)
+            {
+                int newHeight = controlSize.Height;
+                int newWidth = (int)(imageSize.Width * (newHeight / (double)imageSize.Height));
+                return new Size(newWidth, newHeight);
+            }
+            else
+            //if pic is square
+            {
+                return controlSize;
+            }
+        }
+    }
+}
